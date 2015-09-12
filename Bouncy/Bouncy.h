182b4c10adb623cb8917c1de8cc03524cee8b429
Ben Cochran <bcochran@gmail.com>
2015-09-12 16:59:10 -0700

    Initial Commit

---
+++ Bouncy/Bouncy.h
@@ -0,0 +1,19 @@
+//
+//  Bouncy.h
+//  Bouncy
+//
+//  Created by Ben Cochran on 9/12/15.
+//  Copyright © 2015 Ben Cochran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bouncy.
+FOUNDATION_EXPORT double BouncyVersionNumber;
+
+//! Project version string for Bouncy.
+FOUNDATION_EXPORT const unsigned char BouncyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bouncy/PublicHeader.h>
+
+
